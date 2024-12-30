231065b1418a4881c149256878e39df4102891cc
izyan-zubair <asadizyan@email>
2024-12-30 23:29:45 +0500

    commit

---
+++ python class-1/project-1.py
@@ -0,0 +1,20 @@
+name = "izyan zubair"
+age = "15"
+skills = "HTML,CSS,JS"
+
+print(f"my name is {name},my age is {age},my skills are {skills}")
+print("hello everyone")
+print("4")
+print("the number being pribted is", 4)
+print("/nI am learning python")
+
+print("hello everyone")
+print("my name is izyan zubair")
+print("i love playing football")
+print("the sum of 2 + 3", 2 + 3)
+
+print("hello")
+print("my name is izyan zubair")
+print("i love playing football")
+print("i live in pakistan")
+print("the sum of 3 + 7", 3 + 7)
