8b2da70574ff07c1261239c0b3dca6ebc592d6cb
izyan-zubair <asadizyan@email>
2025-01-12 23:47:15 +0500

    commit

---
+++ python calss-6/project-1.py
@@ -0,0 +1,61 @@
+import turtle
+
+pen = turtle.Turtle()
+pen.begin_fill()
+pen.speed(2)
+
+for _ in range(4):
+    pen.forward(100)
+    pen.left(90)
+
+turtle.done()
+
+# import turtle
+# pen = turtle.Turtle()
+# number_of_sides = 10
+# side_length = 100
+
+# for _ in range(number_of_sides):
+#     pen.forward(side_length)
+#     pen.left(360 / number_of_sides)
+# turtle.done()
+
+# import turtle
+
+# pen = turtle.Turtle()
+
+# for _ in range(5):
+#     pen.forward(150)
+#     pen.right(144)
+# turtle.done()
+
+# import turtle
+# pen = turtle.Turtle()
+# pen.circle(100)
+# turtle.done()
+
+
+# import turtle
+# pen = turtle.Turtle()
+# turtle.bgcolor("black")
+
+# pen.color("blue")
+# pen.pensize(3)
+# pen.speed(200)
+# for i in range(100):  
+#     pen.forward(i *2)
+#     pen.right(45)
+# turtle.done()
+
+
+# import turtle
+# pen = turtle.Turtle()
+# turtle.bgcolor("light blue")
+# pen.pensize(3)
+# pen.color("black")
+# pen.speed(5)
+# for _ in range(5):
+#     pen.forward(150)
+#     pen.right(144)
+
+# turtle.done()
